4d51cb4b122e95d24a14da3c6eea16642292a482
Aditi Raj <aditi.raj.tech@gmail.com>
2025-12-25 20:28:21 +0530

    Day 11: Gradient descent implemented

---
+++ week2/day11_calculus_gradient/gradient_descent_scalar.py
@@ -0,0 +1,42 @@
+import numpy as np
+
+print("===GRADIENT DESCENT: SCALAR CASE===")
+
+# FUNCTION
+def f(w):
+      return w**2
+
+# DERIVATIVE OF F(w)
+def grad(w):
+      return 2*w
+
+# START WITH RANDOM W
+w=np.random.randn()
+print("Initial w:",w)
+
+learning_rate=0.1
+
+print("\n===SMALL STEPS (GOOD LEARNING) ===")
+for i in range (20):
+        g=grad(w)
+        w=w-learning_rate*g
+        print(f"Iter {i+1}: w=w{w:.4f},f(w)={f(w):.6f}")
+print("CONVERGED NEAR 0")
+
+#=============================
+#LARGE LEARNING RATE EXPERIMENT
+#=============================
+
+print("\n === LARGE LEARNING RATE (overshooting problem) ===")
+
+w=np.random.randn()
+print("INITIAL w:",w)
+
+learning_rate=1.2  #intentionally big
+
+for i in range (10):
+      g=grad(w)
+      w=w-learning_rate*g
+      print(f"Iter {i+1}: w={w:.4f},f(w)={f(w):.6f}")
+
+print("NOTICE: VALUES JUMP WILDLY INSTEAD OF CONVERGING")
